c833820cf4da50c8a704e31bcc8e1b3b8ef36551
Tai Huynh <anhtaih89@gmail.com>
2016-06-03 17:45:01 -0700

    Add additional styling

--- client/components/Splash.jsx
+++ client/components/Splash.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Splash = function Splash() {
-  return (<div className="splash">
-    <h2> Let's build out the Splash Component </h2>
-  </div>);
+  return (
+    <div className="splash-container">
+      <div className="splash">
+        <h1 className="splash-head">Unplanned</h1>
+        <p className="splash-subhead">Enjoy the moment</p>
+        <p>
+          <button
+            className="pure-button pure-button-primary"
+            onClick={window.socket.api.login}
+          >
+          Login with Facebook
+          </button>
+        </p>
+      </div>
+
+    </div>);
 };
 
 export default Splash;
